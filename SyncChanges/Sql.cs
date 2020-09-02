bb626b17ca30c841779f9a362fa246875b601ce2
D'Arcy Rittich <drittich@gmail.com>
2020-09-01 22:01:04 -0400

    add NormalizeObjectName for comparing table names

--- SyncChanges/Sql.cs
+++ SyncChanges/Sql.cs
@@ -19,5 +19,28 @@ namespace SyncChanges
 			return cn;
 		}
 
+		/// <summary>
+		/// Object names may or may not include schema or quote delimiters ([])
+		/// This normalizes names so we can string compare for equivalence.
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns>A string like '[dbo].[myobjectname]'</returns>
+		public static string NormalizeObjectName(string name, string changeToSchema)
+		{
+			// Let's assume no one would name an object with a period
+			var hasSchema = name.Contains(".");
+
+			if (!hasSchema && string.IsNullOrWhiteSpace(changeToSchema))
+				throw new Exception("Schema must be supplied as part of name or via changeToSchema paramter");
+
+			;
+
+				// remove all square bracket so we start from a known place
+				var normalizedName = name.Replace("[", "").Replace("]", "").ToLowerInvariant();
+
+
+			if (name1 == name)
+		}
+
 	}
 }
